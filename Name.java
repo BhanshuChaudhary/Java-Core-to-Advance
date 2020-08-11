43a88d69b3aebe18ab491aeb135b98df914d8f2e
BhanshuChaudhary <48521811+BhanshuChaudhary@users.noreply.github.com>
2020-08-11 19:42:24 +0530

    Create Name.java

---
+++ Name.java
@@ -0,0 +1,20 @@
+package mynameinhindi;
+
+public class MyNameInHindi 
+{
+    public static void main(String[] args) 
+    {
+        char ch='\u092D';
+        char ch1='\u093E';
+        char ch2='\u0928';
+        char ch3='\u0936';
+        char ch4='\u0941';
+        char ch5='\u091A';
+        char ch6='\u094C';
+        char ch7='\u0927';
+        char ch8='\u0931';
+        char ch9='\u0940';
+        System.out.println("My Name is "+ch+ch1+ch2+ch3+ch4+" "+ch5+ch6+ch7+ch8+ch9);
+    }
+    
+}
